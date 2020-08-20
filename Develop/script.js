7dd3f2faa62996ddc0143d0e67caa56562d60eb0
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-08-20 13:35:29 -0400

    beginning of javascript code

--- Develop/script.js
+++ Develop/script.js
@@ -1,130 +1,65 @@
 // What we want:
   // RANDOMLY generate a pw with certain criteria
 
-    // define variables 
+    // // define arrays
+    // var upperCaseArray = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
+    // var lowerCaseArray = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+    // var numericArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]; 
+    // var specialCaseArray = [ "+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^",
+    // "~", "*", "?", ":"];
+    // var charFiller; 
+    // var userOptionsArray; 
 
-    // define arrays
+    //function to prompt user to input length between 8 and 128 characters, ensuring answer is parsed as an integer, not a string
+    function lengthChoice() {
+      var lengthChoice = parseInt(
+        prompt("Input desired password length:")
+      ); 
 
-    //Generates prompts for pw criteria when click button ("Onclick event")
+        //check to see if input is a number
+      if (isNaN(lengthChoice) === true) {
+        alert("Please input a number value for password length.");
+      return;
+    }
 
-    //Prompt user to input length between 8 and 128 characters
-      //check to see if input is a number
-      //check to see pw is greater than 8 char long
-      //check to see if pw is less than 128 char long
-      //if user does not put in length between 8 and 128 characters
-        //alert that pw needs to be between that length
-        //prompt again
-      //if user does put in length between 8 and 128 characters
-        //store inputLength as a var
-
-      //Ask user to confirm lowercase char inclusion, store answer as var
-
-      //Ask user to confirm uppercase char inclusion, store answer as var
-
-]     //Ask user to confirm numeric char inclusion, store answer as var
-
-      //Ask user to confirm special char inclusion, store answer as var
-
-      // check to see if has at least one of: lowercase, uppercase, numeric, or special char
-        //if not, alert "needs contain..."
-
-      //randomly generate pw using user input length and criteria
-        //only include lowercase, uppercase, special, or numeric that was confirmed as true 
-
-      //display pw as an alert or writtern to page 
-
-
-// eveyrthing below this needs to be edites 
-       
-
-
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// define passwordPrompts
-function passwordPrompts() {
-
-  // vars definition
-  var passwordLength;
-  var lowercase = false;
-  var uppercase = false;
-  var numeric = false;
-  var specialCharacters = false;
-
-  // arrays
-  var upperCaseArray = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
-  var lowerCaseArray = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
-  var numericArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]; 
-  var specialCaseArray = [ "+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^",
-  "~", "*", "?", ":"];
-  var charFiller; 
-  var userOptionsArray; 
-  var password = ""
-
-
-
-  // prompt for length
-
-  // if length is NOT greaterThan 8 and lessThan 128
-      //alert (must have password between 8 and 128 chars)
-
-
-  //confirm: lowercase 
-  //confirm: uppercase
-  //confirm: numeric
-  //confirm: special characters
-
-  // check if at lease one is chosen by the user (true)
-
-
-    // generate passord
-    
-    // if (uppercase)
-      // add first char from (random upper)
-      charFiller = upperCaseArray;
-
-    // if (numeric)
-      // add second char from (random)
-      charFiller = numericArray;
-
-    //if (special)
-      // add third char from (random upper)
-      charFiller = specialCaseArray;
-
-    // if (lowercase)
-      // for each missing letter, add a letter from the lowercase 
-      // for (var i = password.length; i < passwordLength; i++ ) {
-        // add char from lowercase
-      }
-      while(password.length < passwordLength) {
-        // add char from filler
-      }
-
-    
-    //alert (Please choose longer than...) or (shorter length)
-
-
-    // alert password:
-    // return password:
-}
-
-// define generatePassword
-function generatePassword() {
-
-}
-
-// Write password to the #password input
-function writePassword() {
+    //check to see pw is greater than 8 char long and less than 128 char long, prompts again if answer evaluates to false
+    if (lengthChoice !== < 8 && > 128) {
+      alert("Please input a password length between 8 and 128 numbers long.")
+      return;
+    }
   
-  // call generatePassword
-  var password = passwordPrompts ();
-  var passwordText = document.querySelector("#password");
+  //Ask user to confirm lowercase char inclusion, store answer as var
+  var lowerChar = confirm(
+    "Click OK to include lowercase letters in your password."
+  );
 
-  passwordText.value = password;
+  //Ask user to confirm uppercase char inclusion, store answer as var
+  var upperChar = confirm(
+    "Click OK to include uppercase letters in your password."
+  );
 
+  //Ask user to confirm numeric char inclusion, store answer as var
+  var numChar = confirm(
+    "Click OK to include numeric characters in your password."
+  );
+
+//Ask user to confirm special char inclusion, store answer as var
+var specChar = confirm(
+  "Click OK to include special characters in your password."
+);
+
+// check to see if has at least one of: lowercase, uppercase, numeric, or special char
+if (
+  lowerChar === false && upperChar === false && numChar === false && specChar === false
+) {
+  //if not, alert "needs contain..."
+  alert("Your password must contain at least one lowercase letters, uppercase letters, numeric characters, and special characters.");
+  return;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//randomly generate pw using user input length and criteria
+  //only include lowercase, uppercase, special, or numeric choices that were  confirmed as true, using the arrays as reference to generate random values.
+    // add the value to the pw
+
+//display pw as an alert or writtern to page/
+  }
