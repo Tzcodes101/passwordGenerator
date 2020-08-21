6a21d88c14c8edecf61c9e715ae42752e529cc15
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-08-21 08:32:25 -0400

    code up until random pw generation

--- Develop/script.js
+++ Develop/script.js
@@ -1,65 +1,114 @@
 // What we want:
-  // RANDOMLY generate a pw with certain criteria
+// RANDOMLY generate a pw with certain criteria
 
-    // // define arrays
-    // var upperCaseArray = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
-    // var lowerCaseArray = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
-    // var numericArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]; 
-    // var specialCaseArray = [ "+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^",
-    // "~", "*", "?", ":"];
-    // var charFiller; 
-    // var userOptionsArray; 
+// // define arrays and vars
+var lowerCharArr = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+var upperCharArr = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
+var numCharArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]; 
+var specCharArr = [ "+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^",
+"~", "*", "?", ":"];
+var lowerChar;
+var upperChar;
+var numChar;
+var specChar;
+var lengthChoice;
+var randomArr = [];
+var randomPw = "";
+var charSelectionArr = [lowerChar, upperChar, numChar, specChar]
 
-    //function to prompt user to input length between 8 and 128 characters, ensuring answer is parsed as an integer, not a string
-    function lengthChoice() {
-      var lengthChoice = parseInt(
-        prompt("Input desired password length:")
-      ); 
-
-        //check to see if input is a number
-      if (isNaN(lengthChoice) === true) {
-        alert("Please input a number value for password length.");
-      return;
-    }
-
-    //check to see pw is greater than 8 char long and less than 128 char long, prompts again if answer evaluates to false
-    if (lengthChoice !== < 8 && > 128) {
-      alert("Please input a password length between 8 and 128 numbers long.")
-      return;
-    }
-  
-  //Ask user to confirm lowercase char inclusion, store answer as var
-  var lowerChar = confirm(
-    "Click OK to include lowercase letters in your password."
+//function to prompt user to input length between 8 and 128 characters, ensuring answer is parsed as an integer, not a string
+function lengthChoice() {
+  var pwLength = parseInt(
+    prompt("Input desired password length:")
   );
 
-  //Ask user to confirm uppercase char inclusion, store answer as var
-  var upperChar = confirm(
-    "Click OK to include uppercase letters in your password."
+  //check to see if input is a number
+  if (isNaN(pwLength) === true) {
+    alert("Please input a number value for password length.");
+    // have user input length again
+    lengthChoice();
+  }
+
+
+  //check to see pw is greater than 8 char long and less than 128 char long, prompts again if answer evaluates to false
+  if (pwLength < 8 || pwLength > 128) {
+    alert("Please input a password length between 8 and 128 numbers long.")
+    // have user input length again
+    lengthChoice(); 
+  };
+  //call character selection function
+  charConfirm();
+}
+
+//define character selection function
+function charConfirm(){
+
+//Ask user to confirm lowercase char inclusion
+lowerChar = confirm(
+  "Click OK to include lowercase letters in your password."
   );
 
-  //Ask user to confirm numeric char inclusion, store answer as var
-  var numChar = confirm(
-    "Click OK to include numeric characters in your password."
+// if loweChar evaluates to true, then add the lowerCharArr values to randomArr
+if (lowerChar){
+  Array.prototype.push.apply(randomArr, lowerCharArr)
+}
+// check to see if lowerCharArr added to randomArr
+console.log(randomArr);
+
+//Ask user to confirm uppercase char inclusion
+ upperChar = confirm(
+  "Click OK to include uppercase letters in your password."
   );
 
-//Ask user to confirm special char inclusion, store answer as var
-var specChar = confirm(
+// if upperChar evaluates to true, then add the upperCharArr values to randomArr
+if (upperChar){
+  Array.prototype.push.apply(randomArr, upperCharArr)
+}
+// check to see if upperrCharArr added to randomArr
+console.log(randomArr);3
+
+//Ask user to confirm numeric char inclusion
+ numChar = confirm(
+  "Click OK to include numeric characters in your password."
+  );
+
+// if numChar evaluates to true, then add the numCharArr values to randomArr
+if (numChar){
+  Array.prototype.push.apply(randomArr, numCharArr)
+}
+// check to see if numCharArr added to randomArr
+console.log(randomArr);
+
+//Ask user to confirm special char inclusion
+ specChar = confirm(
   "Click OK to include special characters in your password."
-);
+  );
+
+// if specChar evaluates to true, then add the specCharArr values to randomArr
+if (specChar){
+  Array.prototype.push.apply(randomArr, specCharArr)
+}
+// check to see if specCharArr added to randomArr
+console.log(randomArr);
 
 // check to see if has at least one of: lowercase, uppercase, numeric, or special char
 if (
   lowerChar === false && upperChar === false && numChar === false && specChar === false
 ) {
-  //if not, alert "needs contain..."
-  alert("Your password must contain at least one lowercase letters, uppercase letters, numeric characters, and special characters.");
-  return;
+  //alert "needs contain..."
+  alert(
+    "Your password must contain at least one lowercase letter, uppercase letter, numeric character, or special character."
+  ); 
+}
+  // have user make selections again
+  // charConfirm();
 }
 
-//randomly generate pw using user input length and criteria
-  //only include lowercase, uppercase, special, or numeric choices that were  confirmed as true, using the arrays as reference to generate random values.
-    // add the value to the pw
+//Math.floor(Math.random()) to get random characters from randomArr
+//Then add to randomPw
+//randomPw += ......
 
 //display pw as an alert or writtern to page/
-  }
+
+
+lengthChoice(); 
